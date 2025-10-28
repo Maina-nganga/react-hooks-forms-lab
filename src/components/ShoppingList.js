f9c3e189753f99d154e7bfbca3ad1506895c7a40
Maina Nganga <ngangamaina091@gmail.com>
2025-10-28 22:27:13 +0300

    solve few bugs

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,28 +1,22 @@
 import React, { useState } from "react";
-import ItemForm from "./ItemForm";
 import Filter from "./Filter";
-import Item from "./Item";
 
 function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState("");
 
-  function handleCategoryChange(event) {
-    setSelectedCategory(event.target.value);
-  }
-
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
-    return item.category === selectedCategory;
-  });
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter search={search} onSearchChange={setSearch} />
+
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+          <li key={item.id}>
+            <span>{item.name}</span> - <span>{item.category}</span>
+          </li>
         ))}
       </ul>
     </div>
